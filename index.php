a7e32b342d092b74b08f19211683f75d148ecc84
PrimCorp <101891072+PrimCorp@users.noreply.github.com>
2022-03-18 12:51:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GASTRO CORP ECUADOR</title>
+    <link rel="stylesheet" href="css/estilos.CSS">
+  
+    
+    
+    <style>
+        body{
+            background: url(imagen/i1.jpg) ;
+            background-repeat:  no-repeat;
+            background-size: 100%;
+        }
+    </style>
+    
+ </head>
+</head><header ><br>
+    <h1 style="font-size: 50px;"><em><img src="imagen/logo1.png" width="240" height="110";> GASTRO CORP ECUADOR </em>  </h1> 
+
+ </header>  
+ <div id=menus>
+    
+     <ul>
+         <li id="item"><a href="Inicio.php"><font color="white">INICIO</font></a></li>
+         <li id="item"><a href="#"><font color="white">QUIENES SOMOS</font></a>
+            <ul id="desple">
+                <li><a href="Mision.html"><font color="white">MISION</font></a></li> 
+                <li><a href="Vision.html"><font color="white">VISION</font></a></li>
+            </ul>
+        </li>
+         <li id="item"><a href="productos.html"><font color="white"> PRODUCTOS FRIOS</font></a></li>
+         <li id="item"><a href="productosc.html"><font color="white"> PRODUCTOS CALOR</font></a></li>
+         <li id="item"><a href="lineaf.html"><font color="white">GALERIA FRIO</font></a></li>
+         <li id="item"><a href="lineac.html"><font color="white">GALERIA CALOR</font></a></li>
+         <li id="item"><a href="marca.html"><font color="white">NUESTRA MARCA </font></a></li>
+         <li id="item"><a href="#"><font color="white">COMENTARIOS</font></a>
+            
+            <ul id="desple">
+                 
+                <li><a href="index.php"><font color="white">SUGERENCIAS </font></a></li>
+            </ul>
+        </li>
+    
+     </ul>
+    
+
+ </div> <br> <br> <br> <br>
+ 
+ 
+
+
+
+
+  
+
+
+ <body style="background-color:rgb(209, 195, 174)">
+    
+    
+
+<nav>
+
+    <div class="container">
+        
+            <ul class = "slider">
+                <li id="slide1">
+                    <img src="imagen/slider.png"  width="490%" height="520"; >
+                </li>
+                <li id="slide2">
+                    <img src="imagen/slider1.png"  width="490%" height="520"; >
+                </li>                       
+                <li id="slide3">
+                    <img src="imagen/slider2.png"  width="490%" height="520"; >
+                </li>                         
+            </ul>
+
+            <ul class="menu">
+                <li>
+                    <a href="#slide1">1</a>
+                </li>
+                <li>
+                    <a href="#slide2">2</a>
+                </li>
+                <li>
+                    <a href="#slide3">3</a> 
+                </li>
+
+            </ul>
+      
+
+
+      </div>
+              
+
+    
+    
+         
+               
+            <section class="flex-container1">
+                <P class="parrafo3"> <b>OFERTA DE LA SEMANA</b>  </P><font color="white"></font>
+            </section>        
+      
+    <section class="flex-container"> 
+         
+            
+            <p class="parrafo"><b>MAQUINA DE HELADO SOFT 50% <font color="white"></font></b>  <br> <br>
+                <img src="imagen/slider1.png" width="490" height="420";> <br>
+                   
+                </p>
+            
+            
+            <p class="parrafo"> <b>MAQUINA DE HELADO SOFT 50%</b>  <br> <br>
+             <img src="imagen/slider2.png" width="490" height="420"; >
+
+            </p> <br>
+            
+            
+            
+            
+    </section>
+    <section class="contenedor">
+        
+    <footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="imagen/logo.png" width=80" height="160";>
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Somos una empresa dedicada a la importacion de maquinara de alta gama buscado como único objetivo satisfacer las necesidades
+                     y expectativas de nuestros clientes obteniendo así un amplio 
+                    mercado comercial debído a su escala en calidad a diferencia de otras marcas</p>
+                
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <a href="https://www.facebook.com/" ><img src="imagen/facebook.png" width="45" height="45";></a>
+                    <a href="https://www.YouTube.com/" ><img src="imagen/youtube.png" width="45" height="45";></a>
+                    <a href="https://www.Twitter.com/" ><img src="imagen/twitter.png" width="45" height="45";></a>
+                    <a href="https://www.Instagram.com/" ><img src="imagen/instagram.png" width="45" height="45";></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2022 <b>Gastro_Corp Ecuador - contactos: 0978644659 - Email: gastroecuador@ventas.com </b> - Todos los Derechos Reservados.</small>
+        </div>
+    </footer>
+    </section>
+                 
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="https://api.whatsapp.com/send?phone=+593978644659& text= Hola Bienvenido a Nuestra Corporacion, Gastro_CorpEC." class="float" target="_blank">
+    <i class="fa fa-whatsapp my-float"></i>
+    </a>
+      
+
+
+
+
+
+
+
+
+
+
+       
+ </body>
+
+
+ 
+   
+
+</html>
+
+
+
+
